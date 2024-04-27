92de83c0816dae98e315edd277ac0efbcb25d2c5
demka-dd <147042996+demka-dd@users.noreply.github.com>
2024-04-27 13:08:50 +0300

    Версии десктоп и планшетная

--- jquery/slider-reviews.js
+++ jquery/slider-reviews.js
@@ -1,55 +1,55 @@
-// var slider = document.querySelector(".revw");
-// var prev = document.querySelector(".slider-prev");
-// var next = document.querySelector(".slider-next");
-// var slideWidth = document.querySelector(".reviews-item").offsetWidth + 20;
-// var slideNum = 0;
-// var slidesAll = document.querySelectorAll(".reviews-item").length;
+var slider = document.querySelector(".revw");
+var prev = document.querySelector(".slider-prev");
+var next = document.querySelector(".slider-next");
+var slideWidth = document.querySelector(".reviews-item").offsetWidth + 20;
+var slideNum = 0;
+var slidesAll = document.querySelectorAll(".reviews-item").length;
 
-// next.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     if (slideNum < slidesAll - 1) {
-//         slideNum++;
-//     } else {
-//         slideNum = 0;
-//     }
-//     slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
-// });
-
-// prev.addEventListener("click", function (evt) {
-//     evt.preventDefault();
-//     if (slideNum > 0) {
-//         slideNum--;
-//     } else {
-//         slideNum = slidesAll - 1;
-//     }
-//     slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
-// });
-
-
-$(document).ready(function() {
-    var slideNum = 0;
-    var slidesAll = $('.reviews-item').length;
-    var slideWidth = $('.reviews-item').width() + 20;
-
-    $('.slider-next').click(function(evt) {
-        evt.preventDefault();
-        if (slideNum < slidesAll - 1) {
-            slideNum++;
-        } else {
-            slideNum = 0;
-        }
-        $('.reviews').css('transform', 'translateX(-' + slideNum * slideWidth + 'px)');
-    });
-
-    $('.slider-prev').click(function(evt) {
-        evt.preventDefault();
-        if (slideNum > 0) {
-            slideNum--;
-        } else {
-            slideNum = slidesAll - 1;
-        }
-        $('.reviews').css('transform', 'translateX(-' + slideNum* slideWidth + 'px)');
-    });
+next.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (slideNum < slidesAll - 1) {
+        slideNum++;
+    } else {
+        slideNum = 0;
+    }
+    slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
 });
+
+prev.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    if (slideNum > 0) {
+        slideNum--;
+    } else {
+        slideNum = slidesAll - 1;
+    }
+    slider.style.transform = `translateX(-${slideNum * slideWidth}px)`;
+});
+
+
+// $(document).ready(function() {
+//     var slideNum = 0;
+//     var slidesAll = $('.reviews-item').length;
+//     var slideWidth = $('.reviews-item').width() + 20;
+
+//     $('.slider-next').click(function(evt) {
+//         evt.preventDefault();
+//         if (slideNum < slidesAll - 1) {
+//             slideNum++;
+//         } else {
+//             slideNum = 0;
+//         }
+//         $('.reviews').css('transform', 'translateX(-' + slideNum * slideWidth + 'px)');
+//     });
+
+//     $('.slider-prev').click(function(evt) {
+//         evt.preventDefault();
+//         if (slideNum > 0) {
+//             slideNum--;
+//         } else {
+//             slideNum = slidesAll - 1;
+//         }
+//         $('.reviews').css('transform', 'translateX(-' + slideNum* slideWidth + 'px)');
+//     });
+// });
 
 
